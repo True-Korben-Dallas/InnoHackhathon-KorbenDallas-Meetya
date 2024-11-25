f104d43713419be6c004e46a33a1cce7a269c0a7
nicknameplaceholder <127931799+nicknameplaceholder@users.noreply.github.com>
2024-11-25 19:54:45 +0300

    Front branch init (#1)
    * frontend mockup
    
    * deploy test
    
    * deploy test
    
    ---------
    
    Co-authored-by: Yahor <polozov@astra-live>

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { PersonFill } from 'react-bootstrap-icons';
+import styles from './Header.module.scss';
+
+const Header = ({ user }) => {
+    const username = user?.name || 'Guest';
+
+    return (
+        <Navbar bg="white" expand="lg" className={styles.header}>
+            <Container>
+                <Navbar.Brand href="/">Event Management System</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/create-event">Create Event</Nav.Link>
+                        <Nav.Link href="/groups">Users Group</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title={<><PersonFill size={30} /><span className={styles.username}>{username}</span></>} id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
---
+++ frontend/src/constants/tags.js
@@ -0,0 +1,7 @@
+export const tagsList = ['tag1', 'tag2', 'tag3'];
+
+export const tagStyles = {
+    tag1: 'bg-primary text-white',
+    tag2: 'bg-success text-white',
+    tag3: 'bg-warning text-dark',
+};
